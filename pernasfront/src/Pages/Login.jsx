6369b78271fcf2b3b831b76129551ae9d7b9f361
Madu3304 <Mduda0722@gmail.com>
2025-03-24 20:34:37 -0300

    #3691: atualização, estrutura de pastas.

---
+++ pernasfront/src/Pages/Login.jsx
@@ -0,0 +1,65 @@
+import { FaUser, FaLock } from "react-icons/fa"
+import logo from "../assets/logo.png"
+import logo2 from "../assets/logo_sem fundo.png"
+
+import { useState } from "react"
+import "../Styles/Login.css"
+
+const Login = () => {
+    const [ username, setUsername] = useState("")
+    const [ password, setPassword] = useState("")
+
+    const handleSubmit = (Event) => {
+        Event.preventDefault()
+
+        //coletando os dados e informando que está indo para o back. 
+        alert("Salvando usuário:" + username + " - " + password)
+    };
+
+  return (
+    <div>
+      <div className="header">
+      <img src={logo2} alt=""  className="img"/>
+        <h1 className="h1">Pernas  </h1>
+        <h2 className="h2">Solidarias |</h2>
+        <h3 className="h3">Entrar</h3>
+      </div>
+      
+
+    <div className='container'>
+      <form>
+      <img src={logo} alt="" />
+
+        <div className="input-field">
+            <input type="email" placeholder="E-mail"  className="email" onChange={(e) => setUsername(e.target.value)}/>
+            <FaUser className="icon" />
+        </div>
+        <div className="input-field">
+            <input type="password" placeholder="Senha" className="password" onChange={(e) => setPassword(e.target.value)}/>
+            <FaLock className="icon" />
+        </div>
+
+        <div className="recall-forget">
+            <label>
+                <input type="checkbox"/>
+                Lembre de mim
+            </label>
+            <a href="#">Esqueceu a senha?</a>
+        </div>
+
+        <button>Entrar</button>
+
+        <div className="signup-link">
+            <p>
+                Não tem conta? <a href="#">Cadastre-se</a>
+            </p>
+        </div>
+
+
+      </form>
+    </div>
+    </div>
+  )
+}
+
+export default Login
