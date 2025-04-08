e4cd6ae6e72e6469de1d431f2def9d21eb3503b0
maria.souza <maria.souza@fulltechpro.com.br>
2025-04-08 17:03:45 -0300

    #3691:  css da tela de graficos

--- pernasfront/src/Pages/Graficos.jsx
+++ pernasfront/src/Pages/Graficos.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom"
+import "../Styles/Graficos.css"
+import logo from "../assets/logo_sem fundo.png"
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from "recharts";
@@ -21,17 +26,31 @@ const Grafico = () => {
   }, []);
 
   return (
-    <div style={{ width: "100%", height: 400 }}>
-      <ResponsiveContainer>
-        <LineChart data={dados}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="nome" /> {/* ou o campo do seu banco */}
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="quantidade" stroke="#8884d8" /> {/*  */}
-        </LineChart>
-      </ResponsiveContainer>
+    <div>
+       <div className="divPrincipalGraficos">
+      <header>
+        <img src={logo} alt="Logo Pernas Solidárias" />
+        <Link to="/corredor" className="tirarHiperlink">Corredores</Link>
+        <Link to="/cadeirante" className="tirarHiperlink">Cadeirantes</Link>
+        <a>Relatórios</a>
+        <Link to="/evento" className="tirarHiperlink">Evento</Link>
+        <a>Gráficos</a>
+        <a>Membros</a>
+      </header>
+
+      <div style={{ width: "100%", height: 400 }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart data={dados}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="nome" /> {/* substitua pelo campo certo */}
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="quantidade" stroke="#8884d8" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
     </div>
   );
 };
