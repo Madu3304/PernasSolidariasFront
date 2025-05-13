d0fc86d79e60354677d1641e99fa6c086f5cf33e
Madu3304 <Mduda0722@gmail.com>
2025-05-13 19:46:55 -0300

    #3691 - cabeçalho

--- pernasfront/src/Components/Header.jsx
+++ pernasfront/src/Components/Header.jsx
@@ -1,22 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo_sem fundo.png"
-import { toast } from "react-toastify";
-import { useState } from "react";
-import './Header.css'
+import logo from "../assets/logo_sem fundo.png";
+import '../Components/Header.css'; 
 
-const Header = () =>{
-    return(
-        <header>
-            <img src={logo} alt="" />
-            <Link to="/corredor" className="tirarHiperlink">Corredores</Link>
-            <Link to="/cadeirante" className="tirarHiperlink">Cadeirantes</Link>
-            <a>Relatórios</a>
-            <Link to="/evento" className="tirarHiperlink">Evento</Link>
-            <a>Gráficos</a>
-            <a>Membros</a>
-        </header>
-    );
-}
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src={logo} alt="Pernas Solidárias" id="header-logo" />
+      </div>
+      <nav className="header__nav">
+        <ul className="header__list">
+          <li className="header__item">
+            <Link to="/corredor" className="header__link">Corredores</Link>
+          </li>
+          <li className="header__item">
+            <Link to="/cadeirante" className="header__link">Cadeirantes</Link>
+          </li>
+          <li className="header__item">
+            <a href="#" className="header__link">Relatórios</a>
+          </li>
+          <li className="header__item">
+            <Link to="/evento" className="header__link">Evento</Link>
+          </li>
+          <li className="header__item">
+            <a href="#" className="header__link">Gráficos</a>
+          </li>
+          <li className="header__item">
+            <a href="#" className="header__link">Membros</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
